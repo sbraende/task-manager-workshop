5bddc6812983acc39664b3fb1b38cd665d4a81e3
sbraende <sebastian.braende@gmail.com>
2025-02-26 10:02:16 +0100

    Implemnt open and close modal

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+import { closeModal, openModal } from "./modal";
+
+// Selectors
+const formModal = document.querySelector(".form-modal");
+const form = document.querySelector(".form");
+const openModalButton = document.querySelector(".tools__button--add");
+const closeModalButton = document.querySelector(".form__close-button");
+const titleInput = document.querySelector(".form__title-input");
+const dateInput = document.querySelector(".form__date-input");
+const timeInput = document.querySelector(".form__time-input");
+const categorySelect = document.querySelector(".form__category-select");
+const prioritySelect = document.querySelector(".form__priority-select");
+const openChartButton = document.querySelector(".tools__button--chart");
+const filterSelect = document.querySelector(".tools__filter-month");
+const submitButton = document.querySelector(".form__submit-button");
+const formSubmissionFeedback = document.querySelector("form__submission-feedback");
+
+// Event listeners
+document.addEventListener("DOMContentLoaded", () => {
+  openModal(formModal, openModalButton);
+  closeModal(formModal, closeModalButton);
+});
---
+++ src/js/modal.js
@@ -0,0 +1,15 @@
+const openModal = (formModal, openModalButton) => {
+  openModalButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    formModal.classList.add("form-modal--display");
+  });
+};
+
+const closeModal = (formModal, closeModalButton) => {
+  closeModalButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    formModal.classList.remove("form-modal--display");
+  });
+};
+
+export { openModal, closeModal };
